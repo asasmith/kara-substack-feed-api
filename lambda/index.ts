493c5a1ba9b16af33b6d1b924a3ccfb3624ee29d
asasmith <smith.asa.la@gmail.com>
2025-04-30 13:40:42 -0400

    initialize

---
+++ lambda/index.ts
@@ -0,0 +1,68 @@
+import { S3 } from "aws-sdk";
+import * as Parser from "rss-parser";
+
+interface LambdaEvent {
+  httpMethod: string;
+  body?: string;
+}
+
+interface RSSPost {
+  title: string;
+  link: string;
+  pubDate: string;
+  contentSnippet: string;
+}
+
+const s3 = new S3();
+const parser = new Parser();
+
+export const handler = async (event: LambdaEvent) => {
+  try {
+    if (event.httpMethod !== "POST") {
+      return {
+        statusCode: 405,
+        body: JSON.stringify({ message: "method not allowed" }),
+      };
+    }
+
+    const feedUrl = process.env.FEED_URL || "";
+    const bucketName = process.env.BUCKET_NAME || "";
+
+    const feed = await parser.parseURL(feedUrl);
+
+    console.log(`Feed title: ${feed.title}`);
+    console.log(`Feed items: ${feed.items?.length}`);
+
+
+    const posts: RSSPost[] = feed.items.slice(0, 5).map((item: any) => {
+      const { title, link, pubDate, contentSnippet } = item;
+      return {
+        title,
+        link,
+        pubDate,
+        contentSnippet,
+      };
+    });
+
+    await s3
+      .putObject({
+        Bucket: bucketName,
+        Key: "feed.json",
+        Body: JSON.stringify(posts, null, 2),
+        ContentType: "application/json",
+      })
+      .promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: "Feed written to s3" }),
+    };
+  } catch (error) {
+    console.error(`Error processing request: ${error}`);
+
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ message: "Internal server error" }),
+    };
+  }
+};
--- lib/kara-substack-feed-api-stack.ts
+++ lib/kara-substack-feed-api-stack.ts
@@ -1,16 +1,47 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as s3 from "aws-cdk-lib/aws-s3";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigateway from "aws-cdk-lib/aws-apigateway";
 
 export class KaraSubstackFeedApiStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
+        const blockPublicAccess = new s3.BlockPublicAccess({
+            blockPublicAcls: false,
+            blockPublicPolicy: false,
+            ignorePublicAcls: false,
+            restrictPublicBuckets: false,
+        });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'KaraSubstackFeedApiQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+        const bucket = new s3.Bucket(this, "SubstackFeedBucket", {
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+            blockPublicAccess: blockPublicAccess,
+            publicReadAccess: true,
+            websiteIndexDocument: "feed.json",
+        });
+
+        const substackLambda = new lambda.Function(this, "SubstackFeedLambda", {
+            runtime: lambda.Runtime.NODEJS_22_X,
+            code: lambda.Code.fromAsset("lambda"),
+            handler: "index.handler",
+            environment: {
+                BUCKET_NAME: bucket.bucketName,
+                FEED_URL: "https://kararedman.substack.com/feed",
+            },
+        });
+
+        bucket.grantPut(substackLambda);
+
+        new apigateway.LambdaRestApi(this, "SubstackApiGateway", {
+            handler: substackLambda,
+            proxy: true,
+            defaultCorsPreflightOptions: {
+                allowOrigins: apigateway.Cors.ALL_ORIGINS,
+                allowMethods: ["POST"],
+            },
+        });
+    }
 }
