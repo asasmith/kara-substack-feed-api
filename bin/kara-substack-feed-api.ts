da7329bdd3f235f68675595adea5b4c2261064c9
asasmith <smith.asa.la@gmail.com>
2025-05-08 14:54:10 -0400

    feat: Add email sync ci role stack

--- bin/kara-substack-feed-api.ts
+++ bin/kara-substack-feed-api.ts
@@ -2,7 +2,9 @@
 import * as cdk from 'aws-cdk-lib';
 import { KaraSubstackFeedApiStack } from '../lib/kara-substack-feed-api-stack';
 import { SubstackGithubDeployRoleStack } from '../lib/substack-github-deploy-role-stack';
+import { SubstackEmailSyncCIRole } from '../lib/email-sync-ci-role-stack';
 
 const app = new cdk.App();
 new SubstackGithubDeployRoleStack(app, 'SubstackGithubDeployRoleStack');
 new KaraSubstackFeedApiStack(app, 'KaraSubstackFeedApiStack');
+new SubstackEmailSyncCIRole(app, 'SubstackEmailSyncCIRoleStack');
---
+++ lib/email-sync-ci-role-stack.ts
@@ -0,0 +1,46 @@
+import { Arn, Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import * as iam from "aws-cdk-lib/aws-iam";
+
+export class SubstackEmailSyncCIRole extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const oidcProviderArn = Arn.format(
+      {
+        service: "iam",
+        region: "",
+        account: "342811584933",
+        resource: "oidc-provider/token.actions.githubusercontent.com",
+      },
+      this,
+    );
+
+    const oidcProvider = iam.OpenIdConnectProvider.fromOpenIdConnectProviderArn(
+      this,
+      "GithubOIDC",
+      oidcProviderArn,
+    );
+
+    const ciDynamoRole = new iam.Role(this, "SubstackEmailSyncCIRole", {
+      roleName: "SubstackEmailSyncCIRole",
+      assumedBy: new iam.FederatedPrincipal(
+        oidcProvider.openIdConnectProviderArn,
+        {
+          StringLike: {
+            "token.actions.githubusercontent.com:sub":
+              "repo:asasmith/kara-substack-signup:*",
+          },
+        },
+        "sts:AssumeRoleWithWebIdentity",
+      ),
+    });
+
+    ciDynamoRole.addToPolicy(
+      new iam.PolicyStatement({
+        actions: ["dynamodb:scan", "dynamodb:UpdateItem"],
+        resources: [`arn:aws:dynamodb:${this.region}:${this.account}:table/SaturdayPaperEmailsubs`],
+      }),
+    );
+  }
+}
